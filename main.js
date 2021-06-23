68bdc1cf1cb8b8b6a2ff421cd40b8550bd975ce9
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-06-23 14:10:57 +0300

    open and close modal

--- main.js
+++ main.js
@@ -3,12 +3,22 @@ let modal = document.getElementById('simpleModal');
 // get open modal btn
 let modalBtn = document.getElementById('modalBtn');
 // get close btn
-let closeBtn = document.getElementById('closeBtn');
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-// Listen for click
+// Listen for open click
 modalBtn.addEventListener('click', openModal)
+// Listen for close click
+closeBtn.addEventListener('click', closeModal)
+
 
 // function to open modal
 function openModal() {
-    console.log(123)
+    modal.style.display = 'block';
 }
+
+// function to close modal
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+
