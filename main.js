c92dd10106f3ff5febbb0c2f78561d96472a8b6a
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-06-23 14:04:56 +0300

    check openModal func

---
+++ main.js
@@ -0,0 +1,14 @@
+// get modal element
+let modal = document.getElementById('simpleModal');
+// get open modal btn
+let modalBtn = document.getElementById('modalBtn');
+// get close btn
+let closeBtn = document.getElementById('closeBtn');
+
+// Listen for click
+modalBtn.addEventListener('click', openModal)
+
+// function to open modal
+function openModal() {
+    console.log(123)
+}
