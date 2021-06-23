49a065aa1f993131072cd7f7b01149486b71838e
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-06-23 14:13:53 +0300

    outside click

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ let closeBtn = document.getElementsByClassName('closeBtn')[0];
 modalBtn.addEventListener('click', openModal)
 // Listen for close click
 closeBtn.addEventListener('click', closeModal)
-
+// Listen for outside click
+window.addEventListener('click', outsideClick);
 
 // function to open modal
 function openModal() {
@@ -19,6 +20,12 @@ function openModal() {
 // function to close modal
 function closeModal() {
     modal.style.display = 'none';
+}
+// function to close modal if outside click
+function outsideClick(e) {
+    if(e.target == modal) {
+        modal.style.display = 'none';
+    }
 }
 
 
